2019aa8d585d686ee1b23fc6404893c6df7b4342
N00b-AFKXIII <22624239+N00b-AFKXIII@users.noreply.github.com>
2023-01-27 18:28:24 +0100

    Update rsvp form validation

--- assets/js/rsvp.js
+++ assets/js/rsvp.js
@@ -8,6 +8,7 @@ form.addEventListener("submit", function(event) {
     const nameInput = document.getElementById("Name");
     const emailInput = document.getElementById("Email");
     const guestsInput = document.getElementById("Guests");
+    const childrenInput = document.getElementById("Children");
     const messageInput = document.getElementById("message");
 
     //Name is mandatory
@@ -15,16 +16,29 @@ form.addEventListener("submit", function(event) {
         raiseError("Làm ơn điền tên của bạn.");
         return;
     }
-
+    
     //Guest number is mandatory
     if (guestsInput.value.trim() === "") {
         raiseError("Làm ơn cho biết số người đi kèm.");
         return;
     }
 
-    if(emailInput.value.trim() !== "" && !validateEmail(emailInput.value)) {
-        raiseError("Làm ơn điền email hợp lệ.");
+    if (!validateNbGuest(guestsInput.value)) {
+        raiseError("Làm ơn điền số người đi kèm bằng số nhỏ hơn 5.");
         return;
+    }
+
+    if (guestsInput.value > 0) {
+
+        if (childrenInput.value.trim() === "") {
+            raiseError("Làm ơn ghi rõ số lượng trẻ em nếu có.");
+            return;
+        }
+    
+        if (!validateNbGuest(childrenInput.value) || childrenInput.value > guestsInput.value) {
+            raiseError("Làm ơn điền số trẻ em đi kèm bằng số nhỏ hơn hoặc bằng số khách đi kèm.");
+            return;
+        }
     }
 
     submmitForm(this);
@@ -33,6 +47,11 @@ form.addEventListener("submit", function(event) {
 function validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
+}
+
+function validateNbGuest(nbGuest) {
+    var re = /^[0-5]$/;
+    return re.test(String(nbGuest).toLowerCase());
 }
 
 function raiseError(message){
